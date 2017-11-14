a1c8e78220253a509fee861418da71d03cdbb8f4
sasor <s.leamsi99@gmail.com>
2017-11-13 22:21:41 -0500

    vista de login.blade.php ...creado los inputs necesarios

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,5 +1,35 @@
 @extends('master')
 @section('title', 'Login | TBD')
 @section('content')
-    Login View
+
+<form class="columns">
+  <div class="column is-half is-offset-one-quarter">
+    <div class="field">
+      <label class="label">Username</label>
+      <div class="control has-icons-left has-icons-right">
+        <input class="input" type="text" placeholder="username" value="" required>
+        <span class="icon is-small is-left">
+          <i class="fa fa-user"></i>
+        </span>
+      </div>
+    </div>
+    <div class="field">
+      <label class="label">Password</label>
+      <div class="control has-icons-left has-icons-right">
+        <input class="input" type="password" placeholder="password" value="" required>
+        <span class="icon is-small is-left">
+          <i class="fa fa-lock"></i>
+        </span>
+      </div>
+    </div>
+    <div class="field">
+      <p class="control">
+        <button type="submit" class="button is-success">
+          iniciar
+        </button>
+      </p>
+    </div>
+  </div>
+</form>
+
 @endsection
