a26baae73a40c56322aa70db7a74763653f0be6b
sasor <s.leamsi99@gmail.com>
2017-11-19 10:24:55 -0500

    navbar mejorado agregado icon para el usuario autentificado, el icono de la casita te lleva al home

--- resources/views/common/navbar.blade.php
+++ resources/views/common/navbar.blade.php
@@ -2,15 +2,18 @@
   <div class="column">
     <div class="navbar is-info">
       <div class="navbar-brand">
-        <a class="navbar-item">
-          <span><i class="fa fa-home fa-2x"></i><span>
+        <a href="{{ route('home') }}" class="navbar-item">
+          <span><i class="fa fa-home fa-2x"></i>
+          <span>
         </a>
       </div>
       <div class="navbar-menu">
         <div class="navbar-start"></div>
         @if(Auth::check())
           <div class="navbar-end">
-            <span class="navbar-item">{{Auth::User()->username}}</span>
+            <span class="navbar-item">
+              <i class="fa fa-user-circle">&nbsp;</i>{{ Auth::User()->username }}
+            </span>
             <a href="{{ route('logout') }}" class="navbar-item">
               <span><i class="fa fa-sign-out"></i></span>
             </a>
