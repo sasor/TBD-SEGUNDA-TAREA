a690866d7e4ac674cc663aaf2cbdf6a86fed1c5b
sasor <s.leamsi99@gmail.com>
2017-11-23 00:30:29 -0500

    migracion para la tabla session creada y ejecutado migrate

---
+++ database/migrations/2017_11_23_052228_create_session_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSessionTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('session', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('usuario_id');
+            $table->string('token');
+            $table->boolean('activo');
+
+            $table->foreign('usuario_id')->references('id')->on('usuario');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('session');
+    }
+}
